a61dd70f9088f8d94b374cb087b187645a355963
olstra <seriously251@gmail.com>
2024-05-09 13:28:24 +0200

    Feature (footer): Move into general component

---
+++ Repo/src/js/loadFooter.js
@@ -0,0 +1,43 @@
+document.getElementById("footer-placeholder").innerHTML = `
+<footer class="container-fluid bg-dark mt-4 footer">
+    <nav class="container text-center py-3">
+        <ul class="row justify-content-around" style="list-style: none;">
+            <li>
+            <a
+                class="col-12 col-md mr-3 text-white"
+                href="./empty.html?title=About"
+                >About</a
+            >
+            </li>
+            <li>
+            <a
+                class="col-12 col-md mr-3 text-white"
+                href="./empty.html?title=Contact+us"
+                >Contact us</a
+            >
+            </li>
+            <li>
+            <a
+                class="col-12 col-md mr-3 text-white"
+                href="./empty.html?title=Help"
+                >Help</a
+            >
+            </li>
+            <li>
+            <a
+                class="col-12 col-md mr-3 text-white"
+                href="./empty.html?title=Terms+of+services"
+                >Terms of service</a
+            >
+            </li>
+            <li>
+            <a
+                class="col-12 col-md mr-3 text-white"
+                href="./empty.html?title=Privacy+policy"
+                >Privacy policy</a
+            >
+            </li>
+        </ul>
+    </nav>
+</footer>
+`
