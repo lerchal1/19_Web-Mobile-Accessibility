904309b5232a88afe088012270eb18b5145747c2
olstra <seriously251@gmail.com>
2024-05-15 22:35:29 +0200

    Fix (navigation): dropdown menus screen reader

--- Repo/src/js/dropdownMenus.js
+++ Repo/src/js/dropdownMenus.js
@@ -2,8 +2,13 @@ function toggleDropdown(dropdownId) {
     var dropdownToggler = document.getElementById('toggler-' + dropdownId);
     var dropdownMenu = document.getElementById('navbar-' + dropdownId);
 
-    var isExpanded = dropdownToggler.getAttribute('aria-expanded') === 'true';
+    var isExpanded = dropdownToggler.getAttribute('aria-expanded');
 
-    dropdownToggler.setAttribute('aria-expanded', !isExpanded);
-    dropdownMenu.style.display = isExpanded ? 'closed' : 'block';
+    if(isExpanded === 'true') {
+        dropdownToggler.setAttribute('aria-expanded', 'false');
+        dropdownMenu.style.visibility = 'hidden';
+    } else {
+        dropdownToggler.setAttribute('aria-expanded', 'true');
+        dropdownMenu.style.visibility = 'visible';
+    }
 }
--- Repo/src/js/loadNavbar.js
+++ Repo/src/js/loadNavbar.js
@@ -1,7 +1,7 @@
 document.getElementById("navbar-placeholder").innerHTML = `
 <div class="container-fluid bg-dark nav">
     <div class="container">
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg px-0">
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg px-0" role="navigation">
             <div class="navbar-brand d-lg-none">
                 <img class="rounded-right" src="./img/logo.jpg" width="40" height="40"
                     alt="Logo Lorem Ipsum Institute of Technology" />
@@ -12,34 +12,48 @@ document.getElementById("navbar-placeholder").innerHTML = `
                         <a class="nav-link" href="./" tabindex="0">Homepage</a>
                     </ul>
                     <ul class="nav-item dropdown" class="menu-toggle">
-                        <a class="nav-link dropdown-toggle" href="#" id="nav-bar-faculties" tabindex="0">Faculties</a>
-                        <ul class="dropdown-menu">
-                            <li tabindex="0">
-                                <a class="dropdown-item" href="./empty.html?title=Applied+sciences" tabindex="0">Applied
-                                    sciences</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="./empty.html?title=Architecture+%26+planning"
-                                    tabindex="0">Architecture &amp; planning</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="./empty.html?title=Construction+%26+environment"
-                                    tabindex="0">Construction &amp; environment</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="./empty.html?title=Engineering"
-                                    tabindex="0">Engineering</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="./empty.html?title=Health+%26+social+sciences"
-                                    tabindex="0">Health &amp; social sciences</a>
-                            </li>
-                        </ul>
+                    <button class="nav-link dropdown-toggle custom-button" href="#"
+                        id="toggler-faculties"
+                        tabindex="0"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                        onclick="toggleDropdown('faculties')"
+                        >
+                        Faculties
+                    </button>
+                    <ul class="dropdown-menu" id="navbar-faculties" style="visibility: hidden;">
+                        <li>
+                            <a class="dropdown-item" href="./empty.html?title=Applied+sciences" tabindex="0">Applied
+                                sciences</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="./empty.html?title=Architecture+%26+planning"
+                                tabindex="0">Architecture &amp; planning</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="./empty.html?title=Construction+%26+environment"
+                                tabindex="0">Construction &amp; environment</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="./empty.html?title=Engineering"
+                                tabindex="0">Engineering</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="./empty.html?title=Health+%26+social+sciences"
+                                tabindex="0">Health &amp; social sciences</a>
+                        </li>
+                    </ul>
                     </ul>
                     <ul class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="nav-bar-education" tabindex="0">Education &amp;
-                            research</a>
-                        <ul class="dropdown-menu">
+                        <button class="nav-link dropdown-toggle custom-button" href="#" id="toggler-education"
+                        tabindex="0"
+                        aria-haspopup="true" 
+                        aria-expanded="false"
+                        onclick="toggleDropdown('education')"
+                        >
+                        Education &amp; research
+                        </button>
+                        <ul class="dropdown-menu" id="navbar-education" style="visibility: hidden;">
                             <li>
                                 <a class="dropdown-item" href="./empty.html?title=Bachelor" tabindex="0">Bachelor</a>
                             </li>
@@ -56,9 +70,15 @@ document.getElementById("navbar-placeholder").innerHTML = `
                         </ul>
                     </ul>
                     <ul class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="nav-bar-industry" tabindex="0">Industry &amp;
-                            partners</a>
-                        <ul class="dropdown-menu">
+                        <button class="nav-link dropdown-toggle custom-button" href="#" id="toggler-industry"
+                        tabindex="0"
+                        aria-haspopup="true" 
+                        aria-expanded="false"
+                        onclick="toggleDropdown('industry')"
+                        >
+                        Industry &amp; partners
+                        </button>
+                        <ul class="dropdown-menu" id="navbar-industry" style="visibility: hidden;">
                             <li>
                                 <a class="dropdown-item" href="./empty.html?title=Public+relations" tabindex="0">Public
                                     relations</a>
